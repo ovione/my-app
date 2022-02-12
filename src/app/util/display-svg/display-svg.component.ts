7191858d92f4b3b9f943fd558d35edfe991cd499
ovione <ovidio.lairana@gmail.com>
2022-02-12 08:57:25 +0100

    other

--- src/app/util/display-svg/display-svg.component.ts
+++ src/app/util/display-svg/display-svg.component.ts
@@ -6,11 +6,13 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./display-svg.component.css']
 })
 export class DisplaySvgComponent implements OnInit {
+  link: string = '';
   @Input() name!: string;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.link = `/assets/${this.name}.svg#${this.name}`;
   }
 
 }
